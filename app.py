e1c62870a6fd6adaa1c21f0d78ad2c8ad1e4d7a5
Vinay <1nt23cs274.vinay@nmit.ac.in>
2026-05-19 17:50:31 +0530

    testing auto trigger

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "CI/CD Trigger Test Successful!"
+    return "CI/CD Auto Trigger Successful!"
 
 if __name__ == '__main__':
     app.run()
